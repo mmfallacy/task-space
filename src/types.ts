c4c4ff6bc976fac604acb6d5b5dd4a04ac3fa080
splasherzz <juliasabado30@gmail.com>
2023-03-18 22:38:33 +0800

    sprint 2

--- src/types.ts
+++ src/types.ts
@@ -1,7 +1,25 @@
+export interface Badge {
+    badgeName: String;
+    rarity: Int16Array;
+}
+
+export interface Resource {
+    label: String;
+    description: String;
+    classification: String;
+}
+
 export interface Task {
     uid: String;
     name: String;
     category: String;
     deadline: Date;
     timescale: Int16Array;
+}
+
+export interface User {
+    uid: String;
+    name: String;
+    classification: Int16Array;
+    badges: Badge[];
 }
