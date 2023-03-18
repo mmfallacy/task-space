c7c282b21bc12115176fb85e702f58cf60e8ba9e
splasherzz <juliasabado30@gmail.com>
2023-03-18 22:31:21 +0800

    sprint 2

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,9 @@
 <script>
 	import Counter from './Counter.svelte';
-	import Task from './task.svelte';
-	import Button from './button.svelte';
 	import welcome from '$lib/images/svelte-welcome.webp';
 	import welcome_fallback from '$lib/images/svelte-welcome.png';
-
 </script>
 
-<Button>Create a task</Button>
-<Task />
 <svelte:head>
 	<title>Home</title>
 	<meta name="description" content="Svelte demo app" />
