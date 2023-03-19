b7962ddae8c65f4bcce896d9c7d4a47d8699fe2e
splasherzz <juliasabado30@gmail.com>
2023-03-19 20:32:21 +0800

    sprint 2: create a task modal

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,51 @@
-<script>
+<script lang="ts">
 	import Modal from './CreateTaskModal.svelte';
 	let showModal = false;
 
+	// example categories muna yung cat:
+	let cat = [
+		{ id: 1, text: "CS 180" },
+		{ id: 2, text: "CS 192" }
+	];
+
+	let selected;
+	let ans = '';
+
+	// sets task title and date as required fields (idk if needed pa to but i just put it)
+	function isFormValid(data: {[fieldName: string]: any}): boolean {
+    if(!isRequiredFieldValid(data.tasktitle)){
+      return false
+    }
+
+    if(!isRequiredFieldValid(data.date)){
+      return false
+    }
+    return true
+    }
+
+    function isRequiredFieldValid(value){
+    return value != null && value !== ""
+    }
+
 	function onSubmit(e) {
 		// smth here
+		
+		const formData = new FormData(e.target);
+
+		const data: any = {};
+		for (let field of formData) {
+			const [key, value] = field;
+			data[key] = value;
+		}
+
+		//just prints the data in the fields if required fields are filled up (otherwise, says invalid)
+		if(isFormValid(data)){
+			console.log(data)
+
+		} else {
+		console.log("Invalid Form")
+		}
+		
   }
 </script>
 
@@ -20,22 +62,27 @@
 		</h2>
 
 		<form on:submit|preventDefault={onSubmit}>
-			<div>
-				<label for="name">Task Title</label>
+
+			<div> 
+				<label for="name">Task Title</label> 
 				<input
 				  type="text"
+				  id="tasktitle"
 				  name="tasktitle"
 				/>
 			</div>
 			<br />
+
 			<div>
 				<label for="name">Date</label>
 				<input
 				  type=date
+				  id="date"
 				  name="date"
 				/>
 			</div>
 			<br />
+
 			<div>
 				<label for="name">Timeslot</label>
 				<input
@@ -49,14 +96,20 @@
 				/>
 			</div>
 			<br />
+
 			<div>
 				<label for="name">Category</label>
-				<input
-				  type=select
-				  name="category"
-				/>
+				<select value={selected} on:change="{() => ans = ''}">
+					{#each cat as c}
+						<option value={c}>
+						{c.text}
+						</option>
+					{/each}
+				</select>
+
 			</div>
 			<br />
+
 			<div>
 				<label for="name">Notes</label>
 				<input
@@ -65,6 +118,7 @@
 				/>
 			</div>
 			<br />
+
 			<button type="submit">Continue</button>
 	</Modal>
 </main>
