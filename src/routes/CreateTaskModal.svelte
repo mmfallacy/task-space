8de790f90a638cb036197a0a7b3671453b0f91f4
Michael M <31348500+mmfallacy@users.noreply.github.com>
2023-03-20 19:23:53 +0800

    Julia branch (#5)
    * sprint 1 commit
    
    * sprint 1.1 commit
    
    * sprint 1 commit
    
    * sprint 1.1 commit
    
    * sprint 2
    
    * sprint 2
    
    * sprint 2
    
    * sprint 2: entity class to type
    
    * sprint 2: create a task modal
    
    * sprint 2: create a task modal
    
    * sprint 2: create a task modal
    
    * sprint 2: create a task modal
    
    * sprint 2: create a task modal
    
    ---------
    
    Co-authored-by: splasherzz <juliasabado30@gmail.com>

--- src/routes/CreateTaskModal.svelte
+++ src/routes/CreateTaskModal.svelte
@@ -1,24 +1,128 @@
-<script>
-	export let showModal; // boolean
+<script lang="ts">
+	import Modal from './Modal.svelte';
+	
+	let showModal = false;
 
-	let dialog; // HTMLDialogElement
+	// example categories muna yung cat:
+	let cat = [
+		{ id: 1, text: "CS 180" },
+		{ id: 2, text: "CS 192" }
+	];
 
-	$: if (dialog && showModal) dialog.showModal();
+	let selected;
+	let ans = '';
+
+	// sets task title and date as required fields (idk if needed pa to but i just put it)
+	function isFormValid(data: {[fieldName: string]: any}): boolean {
+    if(!isRequiredFieldValid(data.tasktitle)){
+      return false
+    }
+
+    if(!isRequiredFieldValid(data.date)){
+      return false
+    }
+    return true
+    }
+
+    function isRequiredFieldValid(value){
+    return value != null && value !== ""
+    }
+
+	function onSubmit(e) {
+		// smth here
+		
+		const formData = new FormData(e.target);
+
+		const data: any = {};
+		for (let field of formData) {
+			const [key, value] = field;
+			data[key] = value;
+		}
+
+		//just prints the data in the fields if required fields are filled up (otherwise, says invalid)
+		if(isFormValid(data)){
+			console.log(data)
+
+		} else {
+		console.log("Invalid Form")
+		}
+		
+  	}
 </script>
 
-<!-- svelte-ignore a11y-click-events-have-key-events -->
-<dialog
-	bind:this={dialog}
-	on:close={() => (showModal = false)}
-	on:click|self={() => dialog.close()}
->
-	<div on:click|stopPropagation>
-		<slot name="header" />
-		<slot />
-		<!-- svelte-ignore a11y-autofocus -->
-		
-	</div>
-</dialog>
+<main>
+	<button on:click={() => (showModal = true)}>
+		+
+	</button>
+	
+	<Modal bind:showModal>
+		<h2 slot="header">
+			<b>Create a task</b>
+			<p>An organized task schedule is key to success!</p>
+			<hr />
+		</h2>
+
+		<form on:submit|preventDefault={onSubmit}>
+
+			<div> 
+				<label for="name">Task Title</label> 
+				<input
+				  type="text"
+				  id="tasktitle"
+				  name="tasktitle"
+				/>
+			</div>
+			<br />
+
+			<div>
+				<label for="name">Date</label>
+				<input
+				  type=date
+				  id="date"
+				  name="date"
+				/>
+			</div>
+			<br />
+
+			<div>
+				<label for="name">Timeslot</label>
+				<input
+				  type=time
+				  name="startTime"
+				/>
+			<nobr>-</nobr>
+				<input
+				  type=time
+				  name="endTime"
+				/>
+			</div>
+			<br />
+
+			<div>
+				<label for="name">Category</label>
+				<select value={selected} on:change="{() => ans = ''}">
+					{#each cat as c}
+						<option value={c}>
+						{c.text}
+						</option>
+					{/each}
+				</select>
+
+			</div>
+			<br />
+
+			<div>
+				<label for="name">Notes</label>
+				<input
+				  type="text"
+				  name="notes"
+				/>
+			</div>
+			<br />
+
+			<button type="submit">Continue</button>
+	</Modal>
+</main>
 
 <style>
 
---
+++ src/routes/Modal.svelte
@@ -0,0 +1,24 @@
+<script>
+	export let showModal; // boolean
+
+	let dialog; // HTMLDialogElement
+
+	$: if (dialog && showModal) dialog.showModal();
+</script>
+
+<!-- svelte-ignore a11y-click-events-have-key-events -->
+<dialog
+	bind:this={dialog}
+	on:close={() => (showModal = false)}
+	on:click|self={() => dialog.close()}
+>
+	<div on:click|stopPropagation>
+		<slot name="header" />
+		<slot />
+		<!-- svelte-ignore a11y-autofocus -->
+	</div>
+</dialog>
+
+<style>
+
+</style>
