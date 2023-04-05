a27f85db081de251000138e46b49b33a117e6b49
Princess Ventures <princessventures@CessysM1Air.local>
2023-04-05 10:05:38 +0800

    signup page

---
+++ src/routes/Signup/+page.svelte
@@ -0,0 +1,221 @@
+<script>
+	// import './styles.css';
+</script>
+
+<div class="app">
+	<main>
+		<div class="body">
+		<div class="body-left">
+		<div class="loginLeftContainer">
+			<div class="title">
+				<p class="TTCommons-SemiBold-72">Task Space</p>
+				<p class="TTCommons-Regular-20">A safe space to help students plan and perform academic tasks with ease</p>
+			</div>
+			<div>
+				<div class="bannerImage">
+				</div>
+			</div>
+		</div>
+		</div>
+		<div class="loginRightContainer">
+			<div class="RightContainerInner">
+
+				<h1 class="TTCommons-Regular-34">Sign Up</h1>
+
+				<p class="TTCommons-Regular-18">Already have an account? <a class="hyperlink" href="#">Login here.</a></p>
+
+				<div class="name-wrapper">
+					<div class="firstname">
+						<div style="margin: 0px 0px 8px 0px;">
+							<label class="TTCommons-Regular-16" for="name">First Name</label>
+						</div>
+						<div>
+							<input class="form"
+							style="width:280px"
+							type="text"
+							name="email"
+							/>
+						</div>
+					</div>
+					
+
+					<div class="lastname">
+						<div style="margin: 0px 0px 8px 0px;">
+							<label class="TTCommons-Regular-16" for="name">Last Name</label>
+						</div>
+						<div>
+							<input class="form"
+							style="width:280px"
+							type="text"
+							name="email"
+							/>
+						</div>
+					</div>
+				</div>
+				<br />
+
+				<div style="margin: 0px 0px 8px 0px;">
+					<label class="TTCommons-Regular-16" for="name">Email</label>
+				</div>
+				<div>
+					<input class="form"
+					type="text"
+					name="email"
+					/>
+				</div>
+				<br />
+	
+				<div style="margin: 0px 0px 8px 0px;">
+					<label class="TTCommons-Regular-16" for="name">Password</label>
+				</div>
+				<div>
+					<input class="form"
+					type="text"
+					name="password"
+					/>
+				</div>
+				<br />
+	
+				<button class="primary-button" type="submit">Log In</button>
+			</div>
+
+		</div>
+		</div>
+	</main>
+</div>
+
+<style>
+.hyperlink {
+	color: #0A5BD0;
+}
+
+.TTCommons-Regular-34 {
+	font-family: Arial;
+	font-size: 34px;
+	color: black;
+	letter-spacing: -.04em;
+}
+
+.RightContainerInner {
+	align-items: left;
+	width: 40%;
+}
+
+.body {
+	min-height: 100vh;
+    position: relative;
+    background-color: #fff;
+	overflow: hidden;
+    display: flex;
+}
+
+.title {
+	margin: 25px;
+	text-align: center;
+}
+
+.body-left {
+	flex-grow: 0;
+    flex-shrink: 0;
+    flex-basis: 480px;
+}
+
+.loginLeftContainer{
+	height: 100%;
+    background: #F8FAFC;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+}
+
+.bannerImage {
+	display: flex;
+    flex-direction: column;
+    flex-grow: 1;
+    justify-content: flex-end;
+    background-image: url(03.png);
+    background-size: 100%;
+    background-repeat: no-repeat;
+    background-position: bottom;
+    margin: 24px 24px;
+	padding: 150px 24px;
+}
+
+.loginRightContainer{
+	width: 100%;
+    overflow: hidden auto;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    min-height: 100%;
+}
+
+.TTCommons-Regular-16 {
+	font-family: Arial;
+	font-size: 16px;
+	color: black;
+	letter-spacing: -.04em;
+}
+
+.TTCommons-Regular-18 {
+	font-family: Arial;
+	font-size: 18px;
+	color: black;
+	letter-spacing: -.04em;
+}
+
+.TTCommons-Regular-20 {
+	font-family: Arial;
+	font-size: 20px;
+	color: black;
+	letter-spacing: -.04em;
+}
+
+.TTCommons-SemiBold-72 {
+	font-family: Arial;
+	font-size: 72px;
+	color: #04285C;
+	letter-spacing: -.04em;
+}
+
+.primary-button{
+	background-color: #0A5BD0;
+
+	cursor: pointer;
+
+	font-family: Arial;
+    font-weight: 600;
+    border-radius: 5px;
+    box-shadow: none;
+    color: #F8FAFC;
+    line-height: 20px;
+    height: 44px;
+
+	border: 1px solid transparent;
+    width: 100%;
+    font-size: 14px;
+	text-align: center;
+    vertical-align: middle;
+	display: inline-block;
+}
+
+.form{
+	width: 100%;
+	background-color: #E2E8F0;
+	border-radius: 4px;
+    font-family: Arial;
+    padding: 12px 16px;
+    border: 1px solid #E2E8F0;
+    box-sizing: border-box;
+    transition: .5s;
+    outline: none;
+}
+
+.name-wrapper {
+	display: flex;
+    align-items: center;
+    justify-content: space-between;
+	gap: 0px;
+}
+</style>
