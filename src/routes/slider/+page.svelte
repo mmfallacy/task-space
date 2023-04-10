e336504a9a33bb8ad91b676ea9154f740f536f3c
Princess Ventures <princessventures@CessysM1Air.local>
2023-04-10 15:45:20 +0800

    Added slider

---
+++ src/routes/slider/+page.svelte
@@ -0,0 +1,18 @@
+<!-- App.svelte -->
+<script>
+    import RangeInput from './RangeInput.svelte';
+</script>
+  
+<main>
+    <RangeInput min={0} max={100} value={50} label="Slider:" id="slider1" />
+</main>
+  
+<style>
+  main {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-top: 50px;
+  }
+  
+</style>
---
+++ src/routes/slider/RangeInput.svelte
@@ -0,0 +1,56 @@
+<script>
+    export let min = 0;
+    export let max = 100;
+    export let value = 50;
+    export let label = "Slider:";
+    export let id = "slider";
+  
+    function handleInput(event) {
+      value = +event.target.value;
+    }
+  </script>
+  
+  <main>
+    <label for={id}>{label}</label>
+    <input class="RangeInput" type="range" id={id} min={min} max={max} bind:value on:input={handleInput}>
+  </main>
+  
+  <style>
+
+  .RangeInput {
+    width: 100%;
+    height: 10px;
+    background-color: #F1F5F9;
+    border: none;
+    border-radius: 10px;
+    -webkit-appearance: none;
+    position: relative;
+  }
+
+  .RangeInput::-webkit-slider-thumb {
+    -webkit-appearance: none;
+    width: 20px;
+    height: 20px;
+    background-color: #156EF0;
+    border: 1px solid #156EF0;
+    border-radius: 50%;
+    cursor: pointer;
+    position: relative;
+    z-index: 2;
+  }
+
+  main {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-top: 50px;
+  }
+
+  label {
+    font-size: 16px;
+    margin-bottom: 10px;
+  }
+
+
+  </style>
+  
