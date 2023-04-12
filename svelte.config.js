1dc4d43b06ec717877efa47f6dc5efa58a05ca26
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2023-04-12 13:29:13 +0800

    fix: svelte alias __dirname error

--- svelte.config.js
+++ svelte.config.js
@@ -14,7 +14,7 @@ const config = {
         // See https://kit.svelte.dev/docs/adapters for more information about adapters.
         adapter: adapter(),
         alias: {
-            "@": path.resolve(__dirname, './src'),
+            "@": path.resolve('./src'),
         }
     }
 };
