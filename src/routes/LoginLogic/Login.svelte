8afc90d7b06abd0ae9fcfcbafc2efeb1036ad275
JimmyChrome <112991850+JimmyChrome@users.noreply.github.com>
2023-04-12 19:33:10 +0800

    sprint 2 logic

---
+++ src/routes/LoginLogic/Login.svelte
@@ -0,0 +1,29 @@
+<script>
+    let email = '';
+    let password = '';
+    let isLoggedIn = false;
+  
+    const handleLogin = () => {
+      const user = JSON.parse(localStorage.getItem('user'));
+      if (user.email === email && user.password === password) {
+        isLoggedIn = true;
+      }
+    };
+  </script>
+  
+  {#if isLoggedIn}
+    <p>Welcome back!</p>
+  {:else}
+    <form on:submit|preventDefault={handleLogin}>
+      <label>
+        Email:
+        <input type="email" bind:value={email} />
+      </label>
+      <label>
+        Password:
+        <input type="password" bind:value={password} />
+      </label>
+      <button type="submit">Log In</button>
+    </form>
+  {/if}
+  
---
+++ src/routes/LoginLogic/SignUp.svelte
@@ -0,0 +1,31 @@
+<script>
+    let name = '';
+    let email = '';
+    let password = '';
+  
+    const handleSubmit = () => {
+      const user = {
+        name,
+        email,
+        password
+      };
+      localStorage.setItem('user', JSON.stringify(user));
+    };
+  </script>
+  
+  <form on:submit|preventDefault={handleSubmit}>
+    <label>
+      Name:
+      <input type="text" bind:value={name} />
+    </label>
+    <label>
+      Email:
+      <input type="email" bind:value={email} />
+    </label>
+    <label>
+      Password:
+      <input type="password" bind:value={password} />
+    </label>
+    <button type="submit">Sign Up</button>
+  </form>
+  
