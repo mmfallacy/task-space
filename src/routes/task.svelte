6ea2b85a0ddef2a99577fbedeab5ea65447bf884
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2023-04-20 22:28:55 +0800

    style: remove unused svelte ignore

--- src/routes/task.svelte
+++ src/routes/task.svelte
@@ -1,22 +1,21 @@
 <script>
-    export let showModal; // boolean
+  export let showModal; // boolean
 
-    let dialog; // HTMLDialogElement
+  let dialog; // HTMLDialogElement
 
-    $: if (dialog && showModal) dialog.showModal();
+  $: if (dialog && showModal) dialog.showModal();
 </script>
 
 <!-- svelte-ignore a11y-click-events-have-key-events -->
 <dialog
-    bind:this={dialog}
-    on:close={() => (showModal = false)}
-    on:click|self={() => dialog.close()}
+  bind:this={dialog}
+  on:close={() => (showModal = false)}
+  on:click|self={() => dialog.close()}
 >
-    <div on:click|stopPropagation>
-        <slot name="header" />
-        <slot />
-        <!-- svelte-ignore a11y-autofocus -->
-    </div>
+  <div on:click|stopPropagation>
+    <slot name="header" />
+    <slot />
+  </div>
 </dialog>
 
 <style>
