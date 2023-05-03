286360183d7e42254686b71b8509081cfe1a8d2f
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2023-05-03 15:31:33 +0800

    feat: calendar route

---
+++ src/routes/calendar/+page.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+</script>
+
+<div style="position:relative;">
+    <div class="navbar" />
+    <div class="sidebar" />
+    <main class="content">
+        <section class="controls">
+            <h3>Calendar</h3>
+        </section>
+    </main>
+</div>
+
+<style>
+    .navbar {
+        background: #156ef0;
+    }
+
+    .sidebar {
+        background: white;
+        width: 64px;
+        box-shadow: 0px 25px 50px -12px rgba(0, 0, 0, 0.1);
+
+        margin: 10px -8px;
+        padding: 0px;
+    }
+
+    .content {
+        padding-left: 64px;
+    }
+</style>
