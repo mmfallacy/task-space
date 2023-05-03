449746966fcaaedb75b072e66ce118729904382a
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2023-05-03 17:10:15 +0800

    feat: calendar view get days to display of current month

---
+++ src/components/CalendarView.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+    import {
+        startOfMonth,
+        endOfMonth,
+        startOfWeek,
+        endOfWeek,
+        eachDayOfInterval,
+        format,
+    } from 'date-fns';
+    let current = new Date();
+
+    let monthStart = startOfMonth(current);
+    let monthEnd = endOfMonth(current);
+
+    let weekStartOfMonthStart = startOfWeek(monthStart);
+    let weekEndOfMonthEnd = endOfWeek(monthEnd);
+
+    let days = eachDayOfInterval({
+        start: weekStartOfMonthStart,
+        end: weekEndOfMonthEnd,
+    });
+</script>
+
+<main>
+    {days.map((day) => format(day, 'MM-dd-yyyy'))}
+</main>
+
+<style>
+    main {
+        max-width: 500px;
+        word-wrap: break-word;
+    }
+</style>
