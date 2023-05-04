25a99e3850504c5fae1d27f7db914078b4f5d38a
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2023-05-04 21:42:34 +0800

    feat: calendar controls styling and functionalioty

--- src/routes/calendar/+page.svelte
+++ src/routes/calendar/+page.svelte
@@ -1,27 +1,39 @@
 <script lang="ts">
     import CalendarView from '@/components/Calendar/View.svelte';
     import Layout from '@/routes/layout.svelte';
+    import Left from 'svelte-icons/md/MdChevronLeft.svelte';
+    import Right from 'svelte-icons/md/MdChevronRight.svelte';
     import { today } from '@/utils';
-    import { addMonths } from 'date-fns';
+    import { addMonths, format } from 'date-fns';
 
     let current = today();
+
+    function nextMonth() {
+        current = addMonths(current, 1);
+    }
+    function prevMonth() {
+        current = addMonths(current, -1);
+    }
+    function thisMonth() {
+        current = today();
+    }
 </script>
 
 <Layout>
     <div class="wrapper">
         <section class="controls">
             <h3>Calendar</h3>
-            <button on:click={() => (current = addMonths(current, -1))}>
-                Prev
-            </button>
-            <button on:click={() => (current = addMonths(current, 1))}>
-                Next
-            </button>
         </section>
         <section class="view">
             <header>
-                <h3>November</h3>
-                <button on:click={() => (current = today())}> Today </button>
+                <button on:click={prevMonth}>
+                    <Left />
+                </button>
+                <button on:click={nextMonth}>
+                    <Right />
+                </button>
+                <h2>{format(current, 'MMMM')}</h2>
+                <button on:click={thisMonth} class="today"> Today </button>
             </header>
             <CalendarView {current} />
         </section>
@@ -45,5 +57,35 @@
     }
     .view header {
         width: 100%;
+        display: flex;
+        padding: 8px;
+        align-items: center;
+    }
+
+    .view header button:has(svg) {
+        background: transparent;
+        border: none;
+        margin: 0 8px;
+        color: #475569;
+        width: 24px;
+
+        cursor: pointer;
+    }
+    .view header h2 {
+        font-weight: 600;
+        font-size: 20px;
+        line-height: 24px;
+        flex: 1;
+    }
+
+    .view header .today {
+        padding: 8px 16px;
+        border: 1px solid #e2e8f0;
+        border-radius: 4px;
+        color: #156ef0;
+        font-size: 16px;
+        line-height: 20px;
+
+        cursor: pointer;
     }
 </style>
