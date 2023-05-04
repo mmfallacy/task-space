8500606d649c22f4d3f479efeca0aac0aee11ac2
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2023-05-04 18:09:14 +0800

    feat: add tasks on viewitem and different color for current day

--- src/components/Calendar/View.svelte
+++ src/components/Calendar/View.svelte
@@ -1,16 +1,36 @@
 <script lang="ts">
-    import { getCalendarDaysOfMonth } from '@/utils';
-    import { addMonths, isSameMonth } from 'date-fns';
+    import { getCalendarDaysOfMonth, today } from '@/utils';
+    import { addMonths, isSameDay, isSameMonth, isToday } from 'date-fns';
     import ViewItem from './ViewItem.svelte';
+    import { tasks } from '@/stores/task';
 
-    let current = new Date();
+    let current = today();
     $: days = getCalendarDaysOfMonth(current);
+
+    console.log(
+        current,
+        $tasks.map((task) => ({
+            title: task.name,
+            isNow: isSameDay(task.deadline, current),
+        }))
+    );
+
+    function getVariant(day: Date, current: Date) {
+        if (isToday(day)) return 'today';
+        if (isSameMonth(day, current)) return 'active';
+
+        return 'inactive';
+    }
 </script>
 
 <main>
     <div class="grid">
         {#each days as day}
-            <ViewItem date={day} active={isSameMonth(day, current)} />
+            <ViewItem
+                date={day}
+                variant={getVariant(day, current)}
+                tasks={$tasks.filter((task) => isSameDay(task.deadline, day))}
+            />
         {/each}
     </div>
 
--- src/components/Calendar/ViewItem.svelte
+++ src/components/Calendar/ViewItem.svelte
@@ -1,21 +1,32 @@
 <script lang="ts">
+    import type { TaskType } from '@/types';
     import { format } from 'date-fns';
+    import clsx from 'clsx';
 
     export let date: Date;
-    export let active: boolean;
+    export let variant: string;
+    export let tasks: TaskType[];
 </script>
 
-<div class="item" class:active>
+<div class={clsx(variant, 'item')}>
     {format(date, 'd')}
+    {#each tasks as task}
+        <p>T:{task.name}</p>
+    {/each}
 </div>
 
 <style>
     .item {
         height: 64px;
         width: 64px;
+    }
+    .inactive {
         background: darkgray;
     }
     .active {
         background: white;
+    }
+    .today {
+        background: lightblue;
     }
 </style>
