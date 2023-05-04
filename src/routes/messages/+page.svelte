727df487c048fed296fe79db279a46b18fe78b16
Jimmy <jimoscares@gmail.com>
2023-05-04 23:02:08 +0800

    making toasts appear

---
+++ src/routes/messages/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+	import {notifications} from './notifications.js'
+	import Toast from './Toast.svelte'
+	
+</script>
+<button on:click={() => notifications.email('Invalid Credentials!', 3000)}>
+	Email
+</button>
+<button on:click={() => notifications.password('Invalid Credentials!', 3000)}>
+	Password
+</button>
+<button on:click={() => notifications.registered('Email already registered!', 3000)}>
+	Registered
+</button>
+<Toast />
