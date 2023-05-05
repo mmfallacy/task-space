6e33db8a5d2ce52529abef122c26dca319c41843
Princess Ventures <princessventures@CessysM1Air.local>
2023-05-05 09:18:10 +0800

    Fix: lint and format

--- src/routes/sandbox/+page.svelte
+++ src/routes/sandbox/+page.svelte
@@ -1,7 +1,5 @@
 <script lang="ts">
-    import Modal from '@/components/CreateTaskModal-OLD.svelte';
     import { tasks } from '@/stores/task';
-    import { hashSHA256 } from '@/utils';
     // This code snippet adds a new task with given task properties.
     tasks.add({
         name: 'Task test',
