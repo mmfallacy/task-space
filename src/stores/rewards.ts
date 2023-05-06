ac575f8de6139bd4c444c64f9aa6ffba7f02a9e5
splasherzz <juliasabado30@gmail.com>
2023-05-06 18:08:52 +0800

    feat: added resource & reward type & their content

---
+++ src/stores/rewards.ts
@@ -0,0 +1,42 @@
+
+export const Rewards = [
+    { title: "30 days streak",
+    dateAcquired: new Date(),
+    description: "Way to go! We're so proud of you :)"},
+
+    { title: "20 days streak",
+    dateAcquired: new Date(),
+    description: "It may seem impossible until it's done"},
+
+    { title: "10 days streak",
+    dateAcquired: new Date(),
+    description: "We are capable of amazing things"},
+
+    { title: "5 days streak",
+    dateAcquired: new Date(),
+    description: "It is the courage to continue that counts"},
+
+    { title: "Completed 5 resources",
+    dateAcquired: new Date(),
+    description: ""},
+
+    { title: "Clear all tasks in a day",
+    dateAcquired: new Date(),
+    description: "Good job! Now take a well-deserved break :)"},
+
+    { title: "Completed 5 tasks in a day",
+    dateAcquired: new Date(),
+    description: "What a surge of productivity!"},
+
+    { title: "Completed 3 tasks in a day",
+    dateAcquired: new Date(),
+    description: "You're almost there!"},
+
+    { title: "Complete your first ever task",
+    dateAcquired: new Date(),
+    description: "Welcome to Task Space!"},
+
+    { title: "Get 5 badges",
+    dateAcquired: new Date(),
+    description: "Wow! Seems like you're on a roll :)"}
+]
