00c879c1200f5dd23d1083edf16a65ed132b16be
Princess Ventures <princessventures@CessysM1Air.local>
2023-05-09 22:16:52 +0800

    Added rewards page with component

---
+++ src/components/Rewards.svelte
@@ -0,0 +1,54 @@
+<script>
+    export let reward = {};
+</script>
+
+<div class="notification-container">
+    <div class="notification-reward-image" />
+    <div class="notification-reward-content">
+        <p class="TTCommons-Regular-16">
+            <b>{reward.title}</b> — {reward.content}
+        </p>
+        <p class="notification-reward-content-date">{reward.date}</p>
+    </div>
+</div>
+
+<style>
+    .notification-reward-content-date {
+        font-family: Arial;
+        font-size: 16px;
+        letter-spacing: -0.04em;
+
+        color: #94a3b8;
+    }
+
+    .notification-reward-content {
+        flex: 1 1 auto !important;
+    }
+
+    .notification-reward-image {
+        background-image: url('Avatar.png');
+
+        background-repeat: no-repeat;
+        background-position: top;
+        background-size: 50px;
+        margin: 20px;
+        padding: 25px;
+
+        border: none;
+    }
+
+    .notification-container {
+        display: flex;
+        align-items: center;
+        padding: 16px;
+        border-top: 1px solid #e2e8f0;
+        gap: 32px;
+    }
+
+    .TTCommons-Regular-16 {
+        font-family: Arial;
+        font-size: 16px;
+        color: black;
+        letter-spacing: -0.04em;
+    }
+</style>
