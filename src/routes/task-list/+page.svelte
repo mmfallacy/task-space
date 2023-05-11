965f3808ffbf9511e2daaece1be950e769c34eaa
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2023-05-11 14:11:28 +0800

    feat: remove categories box temporarily and fix styling of tasklist

--- src/routes/task-list/+page.svelte
+++ src/routes/task-list/+page.svelte
@@ -9,143 +9,9 @@
 <Layout>
     <div class="right-left-wrapper">
         <div class="left">
-            <h1 class="TTCommons-Regular-20">Back to Dashboard</h1>
-
-            <button class="line" style="width: 250px;" />
-
-            <div class="checkbox-list">
-                <div class="checkbox-main">
-                    <p class="TTCommons-Regular-16" style="color:white">
-                        All Tasks
-                    </p>
-
-                    <div class="checkbox-container">
-                        <input type="checkbox" />
-                    </div>
-                </div>
-
-                <div class="checkbox-main-white">
-                    <div class="checkbox-child-title">
-                        <p>Type</p>
-                    </div>
-
-                    <div class="flex-fill" />
-
-                    <div class="checkbox-container">
-                        <input type="checkbox" />
-                    </div>
-                </div>
-
-                <div class="horizontal" />
-
-                <div
-                    class="checkbox-main-white"
-                    style="margin-top: -15px; margin-bottom: -15px;"
-                >
-                    <p class="TTCommons-Regular-16">Quiz</p>
-
-                    <div class="flex-fill" />
-
-                    <div class="checkbox-container">
-                        <input type="checkbox" />
-                    </div>
-                </div>
-
-                <div
-                    class="checkbox-main-white"
-                    style="margin-top: -20px; margin-bottom: -15px;"
-                >
-                    <p class="TTCommons-Regular-16">Project</p>
-
-                    <div class="flex-fill" />
-
-                    <div class="checkbox-container">
-                        <input type="checkbox" />
-                    </div>
-                </div>
-
-                <div
-                    class="checkbox-main-white"
-                    style="margin-top: -20px; margin-bottom: -15px;"
-                >
-                    <p class="TTCommons-Regular-16">Problem Set</p>
-
-                    <div class="flex-fill" />
-
-                    <div class="checkbox-container">
-                        <input type="checkbox" />
-                    </div>
-                </div>
-
-                <div class="checkbox-main-white">
-                    <div class="checkbox-child-title">
-                        <p>Categories</p>
-                    </div>
-
-                    <div class="flex-fill" />
-
-                    <div class="checkbox-container">
-                        <input type="checkbox" />
-                    </div>
-                </div>
-
-                <div class="horizontal" />
-
-                <div
-                    class="checkbox-main-white"
-                    style="margin-top: -15px; margin-bottom: -15px;"
-                >
-                    <p class="TTCommons-Regular-16">CS 140</p>
-
-                    <div class="flex-fill" />
-
-                    <div class="checkbox-container">
-                        <input type="checkbox" />
-                    </div>
-                </div>
-
-                <div
-                    class="checkbox-main-white"
-                    style="margin-top: -15px; margin-bottom: -15px;"
-                >
-                    <p class="TTCommons-Regular-16">CS 32</p>
-
-                    <div class="flex-fill" />
-
-                    <div class="checkbox-container">
-                        <input type="checkbox" />
-                    </div>
-                </div>
-
-                <div
-                    class="checkbox-main-white"
-                    style="margin-top: -15px; margin-bottom: -15px;"
-                >
-                    <p class="TTCommons-Regular-16">CS 191</p>
-
-                    <div class="flex-fill" />
-
-                    <div class="checkbox-container">
-                        <input type="checkbox" />
-                    </div>
-                </div>
-
-                <div
-                    class="checkbox-main-white"
-                    style="margin-top: -15px; margin-bottom: -15px;"
-                >
-                    <p class="TTCommons-Regular-16">CS 21</p>
-
-                    <div class="flex-fill" />
-
-                    <div class="checkbox-container">
-                        <input type="checkbox" />
-                    </div>
-                </div>
-            </div>
+            <h1 style="font-family:arial; margin-bottom: 0px;">Task List</h1>
         </div>
         <div class="right">
-            <h1 style="font-family:arial; margin-bottom: 0px;">Task List</h1>
             <h4 style="font-family:arial; margin-top: 8px;">
                 The key is to do the more urgent ones first!
             </h4>
@@ -348,21 +214,20 @@
 
     .horizontal-line {
         border-bottom: 1px solid #e2e8f0;
-        margin: 12px -12px;
     }
     .tasklist-board-header {
         display: flex;
         justify-content: space-between;
         align-items: center;
-        min-height: 24px;
+        padding: 16px;
     }
 
     .tasklist-board-container {
         width: 100%;
-        padding: 12px;
         box-shadow: rgba(0, 0, 0, 0.05) 0px 4px 6px -1px;
         border-radius: 8px;
         background-color: rgb(255, 255, 255);
+        margin: 16px 0;
     }
 
     .tasklist-board {
@@ -386,89 +251,6 @@
         padding: 24px;
     }
 
-    .horizontal {
-        margin-top: -20px;
-        height: 1px;
-        width: 100%;
-        background: #e2e8f0;
-    }
-
-    .checkbox-child-title {
-        font-family: Arial;
-        font-weight: 600;
-        font-size: 14px;
-        line-height: 18px;
-        letter-spacing: 0.08em;
-        text-transform: uppercase;
-        color: #94a3b8;
-
-        height: 100%;
-        display: flex;
-        align-items: center;
-        cursor: pointer;
-        gap: 8px;
-    }
-
-    .flex-fill {
-        flex: 1 1 auto !important;
-    }
-
-    .checkbox-container {
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        position: relative;
-        border-radius: 3px;
-        margin-bottom: 0px;
-    }
-
-    input[type='checkbox'] {
-        box-sizing: border-box;
-        padding: 0;
-    }
-
-    input {
-        color: #000;
-
-        overflow: visible;
-
-        margin: 0;
-        font-family: inherit;
-        font-size: inherit;
-        line-height: inherit;
-    }
-
-    .checkbox-list {
-        border: 1px solid #e2e8f0;
-        margin: 8px 0 0 0px;
-        padding: 14px;
-        border-radius: 5px;
-        box-shadow: 0 10px 15px -3px rgba(0, 0, 0, 0.05);
-
-        display: flex;
-        flex-direction: column;
-        gap: 16px;
-    }
-
-    .checkbox-main {
-        background: #000;
-        box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.05);
-        border-radius: 5px;
-        height: 42px;
-        padding: 0 8px;
-        display: flex;
-        align-items: center;
-    }
-
-    .checkbox-main-white {
-        background: white;
-        border-radius: 5px;
-        padding: 0 8px;
-        margin-bottom: 0px;
-        display: flex;
-        align-items: center;
-    }
-
     .left {
         width: 250px;
         height: 100%;
@@ -479,24 +261,9 @@
         flex-direction: column;
     }
 
-    .line {
-        cursor: pointer;
-
-        background: E2E8F0;
-        border-radius: 8px;
-        border: none;
-    }
-
     .TTCommons-Regular-16 {
         font-family: Arial;
         font-size: 16px;
-        color: black;
-        letter-spacing: -0.04em;
-    }
-
-    .TTCommons-Regular-20 {
-        font-family: Arial;
-        font-size: 20px;
         color: black;
         letter-spacing: -0.04em;
     }
