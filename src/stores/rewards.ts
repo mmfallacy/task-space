6275f7acbbbb5c1224427c30a743d1d3a8f1a5d0
splasherzz <juliasabado30@gmail.com>
2023-05-19 00:33:26 +0800

    feat: add 'uid' and 'userId' to REWARDS RewardType

--- src/stores/rewards.ts
+++ src/stores/rewards.ts
@@ -2,62 +2,82 @@ import { RewardSchema, type RewardType } from '@/types';
 import { z } from 'zod';
 import { persistent } from './persistent';
 
-export const REWARDS = [
+export const REWARDS: RewardType[] = [
     {
+        uid: '',
+        userId: '',
         title: '30 days streak',
         dateAcquired: new Date(0),
         description: "Way to go! We're so proud of you :)",
     },
 
     {
+        uid: '',
+        userId: '',
         title: '20 days streak',
         dateAcquired: new Date(0),
         description: "It may seem impossible until it's done",
     },
 
     {
+        uid: '',
+        userId: '',
         title: '10 days streak',
         dateAcquired: new Date(0),
         description: 'We are capable of amazing things',
     },
 
     {
+        uid: '',
+        userId: '',
         title: '5 days streak',
         dateAcquired: new Date(0),
         description: 'It is the courage to continue that counts',
     },
 
     {
+        uid: '',
+        userId: '',
         title: 'Completed 5 resources',
         dateAcquired: new Date(0),
         description: 'Great job!',
     },
 
     {
+        uid: '',
+        userId: '',
         title: 'Clear all tasks in a day',
         dateAcquired: new Date(0),
         description: 'Good job! Now take a well-deserved break :)',
     },
 
     {
+        uid: '',
+        userId: '',
         title: 'Completed 5 tasks in a day',
         dateAcquired: new Date(0),
         description: 'What a surge of productivity!',
     },
 
     {
+        uid: '',
+        userId: '',
         title: 'Completed 3 tasks in a day',
         dateAcquired: new Date(0),
         description: "You're almost there!",
     },
 
     {
+        uid: '',
+        userId: '',
         title: 'Complete your first ever task',
         dateAcquired: new Date(0),
         description: 'Welcome to Task Space!',
     },
 
     {
+        uid: '',
+        userId: '',
         title: 'Get 5 badges',
         dateAcquired: new Date(0),
         description: "Wow! Seems like you're on a roll :)",
