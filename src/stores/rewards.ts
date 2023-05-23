55f0ee5bbaee4352c5a3514b323ccf02543b5d57
splasherzz <juliasabado30@gmail.com>
2023-05-23 13:54:46 +0800

    feat: display Rewards store in Rewards page

--- src/stores/rewards.ts
+++ src/stores/rewards.ts
@@ -2,64 +2,6 @@ import { RewardSchema, type RewardType } from '@/types';
 import { z } from 'zod';
 import { persistent } from './persistent';
 
-export const REWARDS: RewardType[] = [
-    {
-        uid: '',
-        userId: '',
-        title: 'Completed 20 tasks',
-        dateAcquired: new Date(0),
-        description: "Way to go! We're so proud of you :)",
-    },
-
-    {
-        uid: '',
-        userId: '',
-        title: 'Completed 15 tasks',
-        dateAcquired: new Date(0),
-        description: 'We are capable of amazing things',
-    },
-
-    {
-        uid: '',
-        userId: '',
-        title: 'Completed 10 tasks',
-        dateAcquired: new Date(0),
-        description: "Good job! Now take a well-deserved break :)",
-    },
-
-    {
-        uid: '',
-        userId: '',
-        title: 'Completed 5 tasks',
-        dateAcquired: new Date(0),
-        description: 'What a surge of productivity!',
-    },
-
-    {
-        uid: '',
-        userId: '',
-        title: 'Completed 3 tasks',
-        dateAcquired: new Date(0),
-        description: "You're almost there!",
-    },
-
-    {
-        uid: '',
-        userId: '',
-        title: 'Complete your first ever task',
-        dateAcquired: new Date(0),
-        description: 'Welcome to Task Space!',
-    },
-
-    {
-        uid: '',
-        userId: '',
-        title: 'Get 5 badges',
-        dateAcquired: new Date(0),
-        description: "Wow! Seems like you're on a roll :)",
-    },
-];
-
 const RewardStore = z.array(RewardSchema);
 
 function createRewardStore() {
