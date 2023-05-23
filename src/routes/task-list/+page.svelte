23ec6164da0c9c08b4047ae29ac9e622850eae2d
JimmyChrome <112991850+JimmyChrome@users.noreply.github.com>
2023-05-23 08:23:48 +0800

    format fix

--- src/routes/task-list/+page.svelte
+++ src/routes/task-list/+page.svelte
@@ -50,62 +50,62 @@
 
                     <TaskListContainer />
 
-                        {#each $tasks as task (task.uid)}
-                            {#if $filterValues.computerScience ? task.category === 'Computer Science' : $filterValues.generalEducation ? task.category === 'General Education' : true}
-                                <div class="tasklist-row-container">
-                                    <div class="tasklist-row-tasktitle">
-                                        <div>
-                                            <p
-                                                style="font-family:arial; font-weight: bold;"
-                                            >
-                                                {task.name}
-                                            </p>
-                                        </div>
-                                    </div>
-
-                                    <div class="tasklist-row-duedate">
-                                        <div>
-                                            <p style="font-family:arial">
-                                                {task.deadline.toString()}
-                                            </p>
-                                        </div>
-                                    </div>
-
-                                    <div class="tasklist-row-category">
-                                        <div class="category-container">
-                                            <div
-                                                class="category-color"
-                                                style="background: rgb(255, 107, 0);"
-                                            />
-                                            <p style="font-family:arial">
-                                                {task.category}
-                                            </p>
-                                        </div>
-                                    </div>
-
-                                    <div class="tasklist-row-status">
-                                        <div>
-                                            <p
-                                                style="font-family:arial; font-weight:bold;  color: #10B981"
-                                            >
-                                                Ongoing
-                                            </p>
-                                        </div>
-                                    </div>
-
-                                    <div class="tasklist-row-dropdown">
-                                        <Dropdown
-                                            on:delete={() =>
-                                                handleDeleteTask(task.uid)}
-                                        />
+                    {#each $tasks as task (task.uid)}
+                        {#if $filterValues.computerScience ? task.category === 'Computer Science' : $filterValues.generalEducation ? task.category === 'General Education' : true}
+                            <div class="tasklist-row-container">
+                                <div class="tasklist-row-tasktitle">
+                                    <div>
+                                        <p
+                                            style="font-family:arial; font-weight: bold;"
+                                        >
+                                            {task.name}
+                                        </p>
                                     </div>
                                 </div>
-                            {/if}
-                        {/each}
-                    </div>
+
+                                <div class="tasklist-row-duedate">
+                                    <div>
+                                        <p style="font-family:arial">
+                                            {task.deadline.toString()}
+                                        </p>
+                                    </div>
+                                </div>
+
+                                <div class="tasklist-row-category">
+                                    <div class="category-container">
+                                        <div
+                                            class="category-color"
+                                            style="background: rgb(255, 107, 0);"
+                                        />
+                                        <p style="font-family:arial">
+                                            {task.category}
+                                        </p>
+                                    </div>
+                                </div>
+
+                                <div class="tasklist-row-status">
+                                    <div>
+                                        <p
+                                            style="font-family:arial; font-weight:bold;  color: #10B981"
+                                        >
+                                            Ongoing
+                                        </p>
+                                    </div>
+                                </div>
+
+                                <div class="tasklist-row-dropdown">
+                                    <Dropdown
+                                        on:delete={() =>
+                                            handleDeleteTask(task.uid)}
+                                    />
+                                </div>
+                            </div>
+                        {/if}
+                    {/each}
                 </div>
             </div>
         </div>
+    </div>
 </Layout>
 
 <style>
